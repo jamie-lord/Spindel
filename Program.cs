af8e879dc01b94f1abb3e51372863126035c5a33
Jamie Lord <me@jamielord.co.uk>
2017-03-23 22:45:32 +0000

    Added scraping functionality

--- Program.cs
+++ Program.cs
@@ -1,37 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Runtime.Serialization.Formatters.Binary;
+using HtmlAgilityPack;
 using RaptorDB;
 
 namespace Spindel
 {
     public class Program
     {
-        private static RaptorDBString db;
+		private static RaptorDB<string> db;
 
         public static void Main(string[] args)
         {
-            db = new RaptorDBString(@".\test", true);
+			db = RaptorDB<string>.Open(@"./test", false);
 
-            Global.SaveTimerSeconds = 5;
+			Global.SaveTimerSeconds = 5;
 
-            var c = db.Count();
-
-            for (long i = 0; i < 10000; i++)
-            {
-                var page = new Page();
-                page.Url = i.ToString();
-                page.Children = new List<Page> {new Page("childPage")};
-
-                db.Set(page.Url, SerializeToByteArray(page.Children));
-            }
-
-
-            byte[] test;
-            db.Get("50", out test);
-
-            var childPage = DeserializeFromByteArray(test);
+			var rootPage = new Page("https://www.theguardian.com/uk");
+			rootPage.GetChildren();
+			db.Set(rootPage.Url, SerializeToByteArray(rootPage.Children));
 
             db.Shutdown();
         }
@@ -55,7 +44,6 @@ namespace Spindel
     }
 
     #region [entities]
-
     [Serializable]
     public class Page
     {
@@ -70,7 +58,44 @@ namespace Spindel
 
         public string Url { get; set; }
         public List<Page> Children { get; set; }
-    }
+		public void GetChildren()
+		{
+			if (Url != null)
+			{
+				if (Children == null)
+				{
+					Children = new List<Page>();
+				}
+				foreach (var link in GetHtml(Url))
+				{
+					Children.Add(new Page(link));
+				}
+			}
+		}
 
+		private static List<string> GetHtml(string url)
+		{
+			HtmlDocument htmlDoc = new HtmlDocument();
+			htmlDoc.OptionFixNestedTags = true;
+			HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+			request.Method = "GET";
+			request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0";
+			request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+			request.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-us,en;q=0.5");
+			WebResponse response = request.GetResponse();
+			htmlDoc.Load(response.GetResponseStream(), true);
+			var links = new List<string>();
+			foreach (HtmlNode hrefs in htmlDoc.DocumentNode.SelectNodes("//a[@href]"))
+			{
+				HtmlAttribute att = hrefs.Attributes["href"];
+				foreach (var link in att.Value.Split(' '))
+				{
+					if (link.StartsWith("http", StringComparison.Ordinal) && !links.Contains(link))
+						links.Add(link);
+				}
+			}
+			return links;
+		}
+    }
     #endregion
 }
